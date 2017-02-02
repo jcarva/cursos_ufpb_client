4209d8ce3ad3dd01aa8db8a54c061927ad13b529
Jaelson <jaelsonjunior99@gmail.com>
2017-02-02 14:50:27 -0300

    Create SearchBar component and its duck file

--- app/modules/index.js
+++ app/modules/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import AppTitleReducer from './App/index'
+import FetchCurriculas from './SearchBar/index'
 
 const rootReducer = combineReducers({
-  title: AppTitleReducer
+  curriculas: FetchCurriculas
 });
 
 export default rootReducer;
