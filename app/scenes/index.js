ecc9e85f88c4164824dd2dc16adf789feb7640d9
David Guilherme <davidlguilherme@gmail.com>
2017-02-05 17:01:19 -0500

    Put the root routes in scenes folder

--- app/scenes/index.js
+++ app/scenes/index.js
@@ -1,5 +1,20 @@
-export About from './About/About';
-export AppWrapper from './AppWrapper/AppWrapper';
-export Curriculum from './Curriculum/Curriculum';
-export Home from './Home/Home';
-export NoMatch from './NoMatch/NoMatch';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+// Import scenes
+import About from './About/About';
+import AppWrapper from './AppWrapper/AppWrapper';
+import Curriculum from './Curriculum/Curriculum';
+import Home from './Home/Home';
+import NoMatch from './NoMatch/NoMatch';
+
+export default () => (
+  <Route path="/" component={AppWrapper}>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/curriculum/:id" component={Curriculum}/>
+      <Route path="/about" component={About}/>
+      <Route component={NoMatch}/>
+    </Switch>
+  </Route>
+);
