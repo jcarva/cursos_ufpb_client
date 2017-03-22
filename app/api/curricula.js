4e50a900673edd29777cb77deba31b21e4ac9731
David Guilherme <davidlguilherme@gmail.com>
2017-03-22 15:38:47 -0300

    Create a CurriculaAPI class to connect with remote data

--- app/api/curricula.js
+++ app/api/curricula.js
@@ -4,14 +4,18 @@ import { curriculumSchema } from './schemas';
 
 const BASE_API_URL= 'https://cursos-ufpb.herokuapp.com/api/curricula';
 
-export const getCurricula = () => {
-  return axios
-    .get(`${BASE_API_URL}/${id}`)
-    .then((response) => normalize(response.data, curriculumSchema));
-}
+class CurriculaAPI {
+  getCurricula() {
+    return axios
+      .get(`${BASE_API_URL}`)
+      .then((response) => normalize(response.data.curricula, [curriculumSchema]));
+  }
 
-export const getCurriculum = (id) => {
-  return axios
-    .get(`${BASE_API_URL}/${id}`)
-    .then((response) => normalize(response.data, curriculumSchema));
-}
+  getCurriculum(id) {
+    return axios
+      .get(`${BASE_API_URL}/${id}`)
+      .then((response) => normalize(response.data.curriculum, curriculumSchema));
+  }
+};
+
+export default new CurriculaAPI;
